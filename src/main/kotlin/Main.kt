47e593d48b23c92a11b5c5daefb7afecd2d8c687
David Adamson <david.adamson2304@gmail.com>
2025-08-07 23:44:27 +0200

    feat: block range

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,13 @@
 package io.github.adamsondavid
 
+import org.bukkit.Material
 import org.bukkit.plugin.java.JavaPlugin
 
 class Main : JavaPlugin() {
 
     override fun onEnable() {
         logger.info("mcfs loaded")
+        server.getWorld("world")!!.getChunkAt(1, 2).getBlock(0,-60, 0).type = Material.STONE // -60 is the first layer of air (on the floor)
+        server.getWorld("world")!!.getChunkAt(1, 2).getBlock(0,319, 0).type = Material.STONE // 319 is the max for Y
     }
 }
